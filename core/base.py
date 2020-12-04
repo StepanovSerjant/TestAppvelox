5d9246d9d3957b500b7d9707c5a8b7d1509c4976
StepanovSerjant <56222343+StepanovSerjant@users.noreply.github.com>
2020-12-04 13:19:59 +0300

    Update base.py

--- core/base.py
+++ core/base.py
@@ -1,3 +1,5 @@
-# import for alembic that doesn't see the core module
+# import for alembic that doesn't see the core module and models
 from core.db import Base
+from task_app.models import Task, tasks
+from user_auth.models import User, users
 
